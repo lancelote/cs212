d8bd618671d55c125816f6a9b054ab47a650c30d
lancelote <lancelote.du.lac@gmail.com>
2015-05-11 21:11:59 +0300

    zebra implementation, test suit for im_right

--- test/zebra/test_zebra.py
+++ test/zebra/test_zebra.py
@@ -3,8 +3,19 @@
 # pylint: disable=R0902,C0103,C0111
 
 import unittest
-# import zebra.main
+from zebra.main import im_right
 
 
-class Test(unittest.TestCase):
-    pass
+class TestImRight(unittest.TestCase):
+
+    def test_im_right_returns_correct_result(self):
+        """
+        example house list = [1, 2, 3, 4, 5]
+        """
+        self.assertFalse(im_right(1, 2))
+        self.assertFalse(im_right(2, 3))
+        self.assertFalse(im_right(4, 5))
+        self.assertFalse(im_right(5, 1))
+        self.assertTrue(im_right(3, 2))
+        self.assertTrue(im_right(2, 1))
+        self.assertTrue(im_right(5, 4))
--- zebra/main.py
+++ zebra/main.py
@@ -9,25 +9,45 @@ HOUSES = [1, 2, 3, 4, 5]
 ORDERINGS = list(itertools.permutations(HOUSES))
 
 
-def im_right(h1, h2):
+def im_right(house1, house2):
     """
     Is house 1 immediately right of house 2
     """
-    return h1 - h2 == 1
+    return house1 - house2 == 1
 
 
-def next_to(h1, h2):
+def next_to(house1, house2):
     """
     Are houses next to each other
     """
-    return abs(h1 - h2) == 1
+    return abs(house1 - house2) == 1
 
 
-for (red, green, ivory, yellow, blue) in ORDERINGS:
-    for (englishman, spaniard, ukranian, japanese, norwegian) in ORDERINGS:
-        for (dog, snails, fox, horse, zebra) in ORDERINGS:
-            for (coffee, tea, milk, oj, water) in ORDERINGS:
+def zebra_puzzle():
+    """
+    Return a tuple (water, zebra) indicating their house numbers
+    """
+    houses = first, _, middle, _, _ = [1, 2, 3, 4, 5]
+    orderings = list(itertools.permutations(houses))
+    return next((water, zebra)
+                for (red, green, ivory, yellow, blue) in orderings
+                for (englishman, spaniard, ukranian, japanese,
+                     norwegian) in orderings
+                for (dog, snails, fox, horse, zebra) in orderings
+                for (coffee, tea, milk, orange_juice, water) in orderings
                 for (old_gold, kools, chesterfields, luckystrike,
-                     parliaments) in ORDERINGS:
-                    if (englishman is red):
-                        print("TaDa")
+                     parliaments) in orderings
+                if englishman is red
+                if spaniard is dog
+                if coffee is green
+                if ukranian is tea
+                if im_right(green, ivory)
+                if old_gold is snails
+                if kools is yellow
+                if milk is middle
+                if norwegian is first
+                if next_to(chesterfields, fox)
+                if next_to(kools, horse)
+                if luckystrike is orange_juice
+                if japanese is parliaments
+                if next_to(norwegian, blue))
