f49795a25d4812ea090ad786f028a44d6c2733a4
lancelote <lancelote.du.lac@gmail.com>
2015-05-14 20:00:22 +0300

    next_to test suite

--- test/zebra/test_zebra.py
+++ test/zebra/test_zebra.py
@@ -3,19 +3,31 @@
 # pylint: disable=R0902,C0103,C0111
 
 import unittest
-from zebra.main import right
+from zebra.main import right, next_to
 
 
 class TestImRight(unittest.TestCase):
 
     def test_im_right_returns_correct_result(self):
         """
-        example house list = [1, 2, 3, 4, 5]
+        Example house list = [1, 2, 3, 4, 5]
         """
+        self.assertTrue(right(3, 2))
+        self.assertTrue(right(2, 1))
+        self.assertTrue(right(5, 4))
         self.assertFalse(right(1, 2))
         self.assertFalse(right(2, 3))
         self.assertFalse(right(4, 5))
         self.assertFalse(right(5, 1))
-        self.assertTrue(right(3, 2))
-        self.assertTrue(right(2, 1))
-        self.assertTrue(right(5, 4))
+
+    def test_next_to_returns_correct_result(self):
+        """
+        Example house list = [1, 2, 3, 4, 5]
+        """
+        self.assertTrue(next_to(1, 2))
+        self.assertTrue(next_to(3, 4))
+        self.assertTrue(next_to(4, 5))
+        self.assertTrue(next_to(2, 1))
+        self.assertFalse(next_to(1, 5))
+        self.assertFalse(next_to(5, 1))
+        self.assertFalse(next_to(2, 4))
